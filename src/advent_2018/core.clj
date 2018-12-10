29a9fadb89ee5c39e0583abcb5e19e1913209739
mark <mark@entro-1>
2018-12-10 20:39:36 +0000

    daily commit

--- src/advent_2018/core.clj
+++ src/advent_2018/core.clj
@@ -23,11 +23,80 @@
         (println current-freq)
         (recur (conj seen-set current-freq) (first (rest frequencies)) (rest frequencies))))))
 
+;; Day 9
+
+
+(defn insert-marble
+  [m n player-num]
+  (cond (zero? n)
+        {0 [0 0]
+         :current 0
+         :scores {player-num 0}}
+        (= 1 n) {:current 1 0 [1 1] 1 [0 0] :scores {0 0}}
+        (= 0 (mod n 23))
+        (let [current (:current m)
+              seven-left (-> current m first m first m first m first m first m first m first)
+              eight-left (-> seven-left m first)
+              six-left (-> seven-left m second)
+              working-map (assoc m
+                                 :current six-left
+                                 eight-left [(-> eight-left m first) six-left]
+                                 six-left [eight-left (-> six-left m second)])]
+          (update-in (dissoc working-map seven-left)
+                     [:scores player-num] (fnil + 0) (+ seven-left n)))
+        :else
+        (let
+         [current (:current m)
+          t1 (-> current m second)
+          t2 (-> t1 m second)
+          t2-next (-> t2 m second)]
+          (assoc m
+                 t1 [current n]
+                 t2 [n t2-next]
+                 n [t1 t2]
+                 :current n))))
+
+(defn play-upto [players max-marble]
+  (loop  [marble 0
+          m nil]
+    (when (zero? (mod marble 100000))
+      (println "progress" (/ marble max-marble)))
+    (if (> marble max-marble)
+      m
+      (recur (inc marble) (insert-marble m marble (mod marble players))))))
+
+(defn max-points [m]
+  (apply max (vals (:scores m))))
+
+(play-upto 1 23)
+
+(max-points (play-upto 30 5807))
+
+#_(-> (insert-marble nil 0 0)
+    (insert-marble 1 0)
+    (insert-marble 2 0)
+    (insert-marble 3 0))
+
+
+
+(defn f9 [args]
+  (println "maxpoints="
+   (max-points (apply play-upto (map #(Integer/parseInt %) args)))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [h & args]
-  (case (BigDecimal. h)
+
+
+  (time (f9 ["411" "7117"]))
+  (time (f9 ["411"  "71170"]))
+  (time (f9 ["411"  "711700"]))
+  (time (f9 ["411"  "7117000"]))
+
+  #_(case (BigDecimal. h)
     1.1M (f1 args)
     1.2M (f2 args)
+    9.1M (f9 args)
     )
   )
